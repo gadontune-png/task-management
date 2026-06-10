c0f8d23875de15aa736f3b511579cc00539c142d
gadontune-png <gadontune@gmail.com>
2026-06-10 12:38:02 +0300

    corected the errors

--- validation.py
+++ validation.py
@@ -1,14 +1,13 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    if len(title) == 0:
-        print("Task title cannot be empty.")
+    if len(title) < 1:
         return False
     return True
 
+
 def validate_task_description(description):
-    if len(description) == 0:
-        print("Task description cannot be empty.")
+    if len(description) < 1:
         return False
     return True
 
@@ -17,5 +16,4 @@ def validate_due_date(due_date):
         datetime.strptime(due_date, "%Y-%m-%d")
         return True
     except ValueError:
-        print("Invalid date format. Use YYYY-MM-DD.")
         return False
